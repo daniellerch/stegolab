e8f72bf0dd591962f83e7a2fb17ede12cfb9762c
Daniel Lerch <dlerch@gmail.com>
2020-04-06 21:41:34 +0200

    fix bug in indexation

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -8,7 +8,7 @@ import numpy as np
 import jpeg_toolbox as jt
 
 def H_i(dct, k, l, i):
-    dct_kl = dct[::k+1,::l+1].flatten()
+    dct_kl = dct[::k+8,::l+8].flatten()
     return sum(np.abs(dct_kl) == i)
 
 def beta_kl(dct_0, dct_b, k, l):
@@ -21,13 +21,15 @@ def beta_kl(dct_0, dct_b, k, l):
     return (h01*(hb0-h00) + (hb1-h01)*(h02-h01)) / (h01**2 + (h02-h01)**2)
 
 
-def calibration(path):
+def calibration(path, only_first_channel=False):
     tmpdir = tempfile.mkdtemp()
     predfile = os.path.join(tmpdir, 'img.jpg')
     os.system("convert -chop 4x4 "+path+" "+predfile)
     im_jpeg = jt.load(path)
     impred_jpeg = jt.load(predfile)
-    found = False
+    shutil.rmtree(tmpdir)
+
+    beta_list = []
     for i in range(im_jpeg["jpeg_components"]):
         dct_b = im_jpeg["coef_arrays"][i]
         dct_0 = impred_jpeg["coef_arrays"][i]
@@ -35,15 +37,11 @@ def calibration(path):
         b10 = beta_kl(dct_0, dct_b, 1, 0)   
         b11 = beta_kl(dct_0, dct_b, 1, 1)
         beta = (b01+b10+b11)/3
-        if beta > 0.05:
-            print("Hidden data found in channel "+str(i)+":", beta)
-            found = True
+        beta_list.append(beta)
+        if only_first_channel:
+            break
 
-    if not found:
-        print("No hidden data found", beta)
-
-    shutil.rmtree(tmpdir)
-
+    return beta_list
 
 
 if __name__== "__main__": 
@@ -51,7 +49,13 @@ if __name__== "__main__":
         print("Usage:", sys.argv[0], "<jpeg image>\n")
         sys.exit(0)
 
-    calibration(sys.argv[1])
+    beta_list = calibration(sys.argv[1])
+    found = False
+    for i in range(len(beta_list)):
+        if beta_list[i]>0.01:
+            print("Hidden data found in channel", i, ":", beta_list[i])
+    if not found:
+        print("No hidden data found")
 
 
 
