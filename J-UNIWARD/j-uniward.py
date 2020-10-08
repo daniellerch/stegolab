b61313b7463500302e478d35cb40486c178f8b13
Daniel Lerch <dlerch@gmail.com>
2020-10-08 19:41:20 +0200

    licenses

--- J-UNIWARD/j-uniward.py
+++ J-UNIWARD/j-uniward.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 
+
+# Copyright (c) 2020 Daniel Lerch Hostalot. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+
+# This implementation is based on the paper:
+# Universal Distortion Function for Steganography in an Arbitrary Domain
+# by Vojtěch Holub, Jessica Fridrich and Tomáš Denemark.
+
+
+
 import os
 import sys
 import glob
@@ -10,7 +38,6 @@ import scipy.ndimage
 import numpy as np
 import jpeg_toolbox as jt
 
-#from multiprocessing.dummy import Pool as ThreadPool 
 from multiprocessing import Pool as ThreadPool 
 
 
