38b104f776721b2a5ee548e1162d68200e5cea54
Daniel Lerch <dlerch@gmail.com>
2020-10-09 00:04:18 +0200

    doc/license

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 
-# Implementation based on "Steganalysis of JPEG Images: Breaking the F5 Algorithm", J. Fridrich, 
-# M. Goljan, D. Hogea. (http://www.ws.binghamton.edu/fridrich/Research/f5.pdf).
+# Copyright (c) 2020 Daniel Lerch Hostalot. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+
+# This implementation is based on the paper:
+# Steganalysis of JPEG Images: Breaking the F5 Algorithm 
+# by Jessica Fridrich, Miroslav Goljan and Dorin Hogea.
+# http://www.ws.binghamton.edu/fridrich/Research/f5.pdf
+
+# The tool "convert" from ImageMagick is needed.
+
 
 import os
 import sys
