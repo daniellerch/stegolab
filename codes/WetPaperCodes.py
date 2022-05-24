d3ac1d1d9031bcdd5b7b5f08a45e458be6d7793b
Daniel Lerch <dlerch@gmail.com>
2022-05-24 17:55:43 +0200

    fix date

--- codes/WetPaperCodes.py
+++ codes/WetPaperCodes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.9
 
-# Copyright (c) 2020 Daniel Lerch Hostalot. All rights reserved.
+# Copyright (c) 2022 Daniel Lerch Hostalot. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a 
 # copy of this software and associated documentation files (the "Software"), 
